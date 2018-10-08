8bc836e733a4d8b13a84701e5670a906c61538dd
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-10-08 19:21:03 +0200

    regenerate key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
 import { UserModule } from './modules/user/user.module';
 import { RoutingModule } from './modules/routing/routing.module';
-
-export const firebaseOptions = { 
-    apiKey: "AIzaSyBMZcE9iMzFDY7yabTBqEdiAt-tiQTAOmY",
-    authDomain: "flixenwork.firebaseapp.com",
-    databaseURL: "https://flixenwork.firebaseio.com",
-    projectId: "flixenwork",
-    storageBucket: "flixenwork.appspot.com",
-    messagingSenderId: "364043319613"
-}
+import { firebaseOptions } from '../firebase-options';
 
 @NgModule({
   declarations: [
